b9b62663722524f9b55e9a3bfd7b1c271024bdcd
Christophe Gragnic <cgragnic@netc.fr>
2020-04-15 10:00:47 +0200

    Remove external worker

--- js/metronome.js
+++ js/metronome.js
@@ -333,8 +333,40 @@ function init(){
 
     // if we wanted to load audio files, etc., this is where we should do it.
 
-    timerWorker = new Worker("js/metronomeworker.js");
+    // Build a worker from an anonymous function body
+    var blobURL = URL.createObjectURL( new Blob([ '(',
+    function(){
+        var timerID = null;
+        var interval = 100;
 
+        self.onmessage = function(e){
+            if (e.data == "start") {
+                console.log("starting");
+                timerID = setInterval(
+                        function(){postMessage("tick");},
+                        interval);
+            }
+            else if (e.data.interval) {
+                console.log("setting interval");
+                interval = e.data.interval;
+                console.log("interval = "+interval);
+                if (timerID) {
+                    clearInterval(timerID);
+                    timerID = setInterval(function(){postMessage("tick");},interval)
+                }
+            }
+            else if (e.data == "stop") {
+                console.log("stopping");
+                clearInterval(timerID);
+                timerID = null;
+            }
+        };
+
+        postMessage('hi there');
+    }.toString(),
+    ')()'], {type: 'application/javascript'}));
+
+    timerWorker = new Worker(blobURL);
     timerWorker.onmessage = function(e) {
         if (e.data == "tick") {
             // console.log("tick!");
